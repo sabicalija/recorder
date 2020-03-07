cff3a7ba55087987cee3332057d4cfe883ef2116
Alija Sabic <sabic.alija@gmail.com>
2020-03-07 04:17:27 +0100

    Release: test update

--- js/skip-waiting.js
+++ js/skip-waiting.js
@@ -1,9 +1,7 @@
 addEventListener("message", event => {
-  console.log("message received");
   const replyPort = event.ports[0];
   const message = event.data;
   if (replyPort && message && message.type === "skip-waiting") {
-    console.log("skip waiting ...");
     event.waitUntil(
       self.skipWaiting().then(
         () => replyPort.postMessage({ error: null }),
