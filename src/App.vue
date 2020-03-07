ae85947c2db477d692c43e4bef61593c1245052c
Alija Sabic <sabic.alija@gmail.com>
2020-03-07 01:44:05 +0100

    Cleanup code

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/record">Record</router-link> |
-      <router-link to="/replay">Replay</router-link> |
-      <router-link to="/about">About</router-link> |
+      <router-link to="/">Home</router-link>
+      <router-link to="/record">Record</router-link>
+      <router-link to="/replay">Replay</router-link>
+      <router-link to="/about">About</router-link>
     </div>
     <router-view />
   </div>
@@ -14,9 +14,9 @@
 export default {
   name: "App",
   created() {
-    const storage = localStorage.getItem("input-recorder-records");
-    if (storage) {
-      this.$store.commit("restore-state", storage);
+    const records = localStorage.getItem("input-recorder-records");
+    if (records) {
+      this.$store.commit("restore-state", records);
     }
   }
 };
--- src/components/InputRecorder.vue
+++ src/components/InputRecorder.vue
@@ -1,17 +1,6 @@
 <template>
   <div>
-    <canvas
-      ref="canvas"
-      v-on:mousedown="handleMouseDown"
-      v-on:mouseup="handleMouseUp"
-      v-on:mousemove="handleMouseMove"
-      width="1000px"
-      height="550px"
-    >
-    </canvas>
-    <input :disabled="recorderStatus" type="text" v-model="input" />
-    <button @click="onClick">{{ label }}</button>
-    <button @click="onDownload">Download</button>
+    <canvas></canvas>
   </div>
 </template>
 
@@ -22,112 +11,11 @@ export default {
     return {
       label: "Start",
       recorderStatus: "disabled",
-
-      input: null,
-      mouse: {
-        current: {
-          x: 0,
-          y: 0
-        },
-        down: false
-      },
-      recording: [
-        {
-          windowInnerHeight: window.innerHeight,
-          windowInnerWidth: window.innerWidth
-        }
-      ]
+      input: null
     };
   },
-  computed: {
-    /*******************CANVAS CODE**************  */
-
-    currentMouse: function() {
-      // rect is a DOMRect(rectangle) object with eight properties:
-      // left, top, right, bottom, x, y, width, height
-      const rect = this.$refs.canvas.getBoundingClientRect(); //Assert that c is non-null and access getBoundingClientRect()
-      return {
-        x: this.mouse.current.x - rect.left,
-        y: this.mouse.current.y - rect.top
-      };
-    }
-    /*******************END CANVAS CODE**************  */
-  },
-  methods: {
-    onClick() {
-      if (this.recorderStatus) {
-        this.label = "Stop";
-        this.recorderStatus = false;
-      } else {
-        this.$store.commit("save-record", {
-          scene: this.$route.params.scene,
-          record: this.input,
-          time: new Date()
-        });
-        this.input = "";
-        this.label = "Start";
-        this.recorderStatus = true;
-      }
-    },
-    onDownload() {
-      // const url = window.URL.createObjectURL();
-      const blob = new Blob([this.input], { type: "text/plain" });
-      const link = document.createElement("a");
-      link.href = URL.createObjectURL(blob);
-      link.download = "File-A.txt";
-      link.click();
-      URL.revokeObjectURL(link.href);
-    },
-    /*******************CANVAS CODE**************  */
-
-    draw() {
-      if (this.mouse.down) {
-        const c = this.$refs.canvas;
-        const ctx = c.getContext("2d");
-        ctx.clearRect(0, 0, c.width, c.height);
-        ctx.lineTo(this.currentMouse.x, this.currentMouse.y);
-        ctx.strokeStyle = "blue";
-        ctx.lineWidth = 2;
-        ctx.stroke();
-      }
-    },
-    handleMouseDown(event) {
-      this.mouse.down = true;
-      this.mouse.current = {
-        x: event.pageX,
-        y: event.pageY
-      };
-      const c = this.$refs.canvas;
-      const ctx = c.getContext("2d");
-
-      ctx.moveTo(this.currentMouse.x, this.currentMouse.y);
-    },
-    handleMouseUp() {
-      this.mouse.down = false;
-    },
-    handleMouseMove(event) {
-      this.mouse.current = {
-        x: event.pageX,
-        y: event.pageY,
-        z: Date.now()
-      };
-      if (!this.recorderStatus && this.mouse.down) {
-        this.recording.push({
-          x: this.mouse.current.x,
-          y: this.mouse.current.y - 60,
-          time: this.mouse.current.z
-        });
-        this.draw(event);
-      }
-    }
-  }
-  /*******************END CANVAS CODE**************  */
+  methods: {}
 };
 </script>
 
-<style lang="stylus" scoped>
-canvas {
-  background: white;
-  box-shadow: 0px 2px 3px rgba(0, 0, 0, 0.2);
-}
-</style>
+<style lang="stylus" scoped></style>
